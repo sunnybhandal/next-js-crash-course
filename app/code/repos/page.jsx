4c1c40abd38bd69dc26cb5a95bb3224e03c17e57
sunnybhandal <sunnybhandal123@gmail.com>
2023-05-01 20:47:21 -0500

    reval

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -3,7 +3,10 @@ import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
 
 async function fetchRepos() {
   const response = await fetch(
-    "https://api.github.com/users/sunnybhandal/repos"
+    "https://api.github.com/users/sunnybhandal/repos",
+    {
+      next: { revalidate: 60 },
+    }
   );
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
