1398de8ecd88b406f093fa4a21ee9db868dfc154
sunnybhandal <sunnybhandal123@gmail.com>
2023-05-03 18:11:26 -0500

    add courses

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -4,3 +4,9 @@ import courses from "./data.json";
 export async function GET(request) {
   return NextResponse.json(courses);
 }
+
+export async function POST(request) {
+  const { title, description, level, link } = await request.json();
+  console.log(title, description, level, link);
+  return NextResponse.json({ message: "Course created" });
+}
